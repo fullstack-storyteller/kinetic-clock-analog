226ef8866e3d5bb69900294a5694dbfc9335ab0d
Manish Kumar <manishmatrix@outlook.com>
2021-05-07 02:32:47 +0530

    only synchronization of hour minute and seconds is pending

--- myScript.js
+++ myScript.js
@@ -3,28 +3,48 @@ const animation_delay_quantum_mins = 60;
 const animation_delay_quantum_secs = 1;
 //time quantum after which hours animation will start
 
-const initClockFrame = (delay, z_index, members, identifier) => {
-  for (let i = 0; i < members; i++) {
-    document
-      .querySelector(`.${identifier}${i}`)
-      .style.setProperty('animation-delay', `${delay * i}s`);
+const [hh, mm, ss_daypart] = [...new Date().toLocaleTimeString().split(':')];
+const [ss, daypart] = ss_daypart.split(' ');
+
+const initClockFrame = (delay, z_index, members, identifier, initial) => {
+  //   for (let i = 0; i < members; i++) {
+  //     document
+  //       .querySelector(`.${identifier}${i}`)
+  //       .style.setProperty('animation-delay', `${delay * i}s`);
+
+  //     document
+  //       .querySelector(`.${identifier}${i}`)
+  //       .style.setProperty('z-index', `${z_index - i}`);
+  //   }
+  let i = initial;
+  let counter = 0;
+  while (counter < members) {
+    if (i >= members) {
+      i %= members;
+    }
 
     document
       .querySelector(`.${identifier}${i}`)
-      .style.setProperty('z-index', `${z_index - i}`);
+      .style.setProperty('animation-delay', `${delay * counter}s`);
+
+    document
+      .querySelector(`.${identifier}${i}`)
+      .style.setProperty('z-index', `${z_index - counter}`);
+
+    i++;
+    counter++;
   }
 };
 
-initClockFrame(animation_delay_quantum_hrs, 1300, 12, 'n');
-initClockFrame(animation_delay_quantum_mins, 600, 60, 'm');
-initClockFrame(animation_delay_quantum_secs, 200, 60, 's');
+initClockFrame(animation_delay_quantum_hrs, 1300, 12, 'n', hh);
+initClockFrame(animation_delay_quantum_mins, 600, 60, 'm', mm);
+initClockFrame(animation_delay_quantum_secs, 200, 60, 's', ss);
 
 const localTime = document.querySelector('.local-time span');
 
 const Timer = () => {
   //   x++;
   localTime.textContent = new Date().toLocaleTimeString();
-  //   stop();
 };
 Timer(localTime);
 let start = setInterval(Timer, 1000);
